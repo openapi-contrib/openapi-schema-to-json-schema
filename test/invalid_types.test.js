1dac2ba64060b8472aef14fbc5ae6e74e21b7f58
Mika Kunnas <miksu.kunnas@gmail.com>
2018-04-04 15:30:04 +0300

    Add tests for valid types

--- test/invalid_types.test.js
+++ test/invalid_types.test.js
@@ -27,3 +27,26 @@ test('invalid types', function(assert) {
 	msg = 'invalid type inside complex schema';
 	assert.throws(function() { convert(schema); }, /InvalidTypeError.*invalidtype/, msg);
 });
+
+test('valid types', function(assert) {
+	var types = ['integer', 'number', 'string', 'boolean', 'object', 'array'];
+
+	assert.plan(types.length);
+
+	types.forEach(function(type) {
+		var schema, result, expected;
+
+		schema = {
+			type: type
+		};
+
+		result = convert(schema);
+
+		expected = {
+			$schema: 'http://json-schema.org/draft-04/schema#',
+			type: type,
+		};
+
+		assert.deepEqual(result, expected, type + ' ok');
+	});
+});

de6fcb0cdf253647bbc7dd81048690be39b9a9f2
ehmicky <ehmicky@gmail.com>
2018-05-16 11:54:54 +0200

    Add support for all OpenAPI formats

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ function convertSchema(schema, options) {
 	}
 
 	validateType(schema.type);
-	schema = convertTypes(schema, options);
+	schema = convertTypes(schema);
+	schema = convertFormat(schema, options);
 
 	if (typeof schema['x-patternProperties'] === 'object'
 			&& options.supportPatternProperties) {
@@ -135,27 +136,93 @@ function validateType(type) {
 	}
 }
 
-function convertTypes(schema, options) {
-	var toDateTime = options.dateToDateTime;
-
-	if (schema.type === undefined) {
-		return schema;
-	}
-
-	if (schema.type == 'string' && schema.format == 'date' && toDateTime === true) {
-		schema.format = 'date-time';
-	}
-
-	if (! schema.format) {
-		delete schema.format;
-	}
-
-	if (schema.nullable === true) {
+function convertTypes(schema) {
+	if (schema.type !== undefined && schema.nullable === true) {
 		schema.type = [schema.type, 'null'];
 	}
 
 	return schema;
 }
+
+function convertFormat(schema, options) {
+	var format = schema.format;
+
+	if (format === undefined || FORMATS.indexOf(format) !== -1) {
+		return schema;
+	}
+
+	var converter = formatConverters[format];
+
+	if (converter === undefined) { return schema; }
+
+	return converter(schema, options);
+}
+
+// Valid JSON schema v4 formats
+var FORMATS = ['date-time', 'email', 'hostname', 'ipv4', 'ipv6', 'uri', 'uri-reference'];
+
+function convertFormatInt32 (schema) {
+	schema.minimum = MIN_INT_32;
+	schema.maximum = MAX_INT_32;
+	return schema;
+}
+
+var MIN_INT_32 = 0 - Math.pow(2, 31);
+var MAX_INT_32 = Math.pow(2, 31) - 1;
+
+function convertFormatInt64 (schema) {
+	schema.minimum = MIN_INT_64;
+	schema.maximum = MAX_INT_64;
+	return schema;
+}
+
+var MIN_INT_64 = 0 - Math.pow(2, 63);
+var MAX_INT_64 = Math.pow(2, 63) - 1;
+
+function convertFormatFloat (schema) {
+	schema.minimum = MIN_FLOAT;
+	schema.maximum = MAX_FLOAT;
+	return schema;
+}
+
+var MIN_FLOAT = 0 - Math.pow(2, 128);
+var MAX_FLOAT = Math.pow(2, 128) - 1;
+
+function convertFormatDouble (schema) {
+	schema.minimum = MIN_DOUBLE;
+	schema.maximum = MAX_DOUBLE;
+	return schema;
+}
+
+var MIN_DOUBLE = 0 - Number.MAX_VALUE;
+var MAX_DOUBLE = Number.MAX_VALUE;
+
+function convertFormatDate (schema, options) {
+	if (options.dateToDateTime === true) {
+		schema.format = 'date-time';
+	}
+
+	return schema;
+}
+
+function convertFormatByte (schema) {
+	schema.pattern = BYTE_PATTERN;
+	return schema;
+}
+
+// Matches base64 (RFC 4648)
+// Matches `standard` base64 not `base64url`. The specification does not
+// exclude it but current ongoing OpenAPI plans will distinguish btoh.
+var BYTE_PATTERN = '^[\\w\\d+\\/=]*$';
+
+var formatConverters = {
+	int32: convertFormatInt32,
+	int64: convertFormatInt64,
+	float: convertFormatFloat,
+	double: convertFormatDouble,
+	date: convertFormatDate,
+	byte: convertFormatByte
+};
 
 function convertPatternProperties(schema, handler) {
 	schema.patternProperties = schema['x-patternProperties'];

01f9f5c16284d3771ea65a8545d2382c5a872f60
ehmicky <ehmicky@gmail.com>
2018-05-16 11:55:43 +0200

    Add tests

--- test/numeric_types.test.js
+++ test/numeric_types.test.js
@@ -2,63 +2,65 @@ var test = require('tape')
 	, convert = require('../')
 ;
 
-test('handles integer types', function(assert) {
+test('handles int32 format', function(assert) {
 	var schema
 		, result
 		, expected
 	;
 
-	assert.plan(2);
+	assert.plan(1);
 
 	schema = {
-		type: 'integer',
-	};
-
-	result = convert(schema);
-
-	expected = {
-		$schema: 'http://json-schema.org/draft-04/schema#',
-		type: 'integer',
-	};
-
-	assert.deepEqual(result, expected, 'integer type untouched');
-
-	schema = {
-		type: 'integer',
-		format: 'int32',
-	};
-
-	result = convert(schema);
-
-	expected = {
-		$schema: 'http://json-schema.org/draft-04/schema#',
 		type: 'integer',
 		format: 'int32'
 	};
 
-	assert.deepEqual(result, expected, 'integer type and format untouched');
+	result = convert(schema);
+
+	expected = {
+		$schema: 'http://json-schema.org/draft-04/schema#',
+		type: 'integer',
+		format: 'int32',
+		minimum: 0 - Math.pow(2, 31),
+		maximum: Math.pow(2, 31) - 1
+	};
+
+	assert.deepEqual(result, expected, 'int32 converted to minimum|maximum');
 });
 
-test('handles number types', function(assert) {
+test('handles int64 format', function(assert) {
 	var schema
 		, result
 		, expected
 	;
 
-	assert.plan(2);
+	assert.plan(1);
 
 	schema = {
-		type: 'number',
+		type: 'integer',
+		format: 'int64'
 	};
 
 	result = convert(schema);
 
 	expected = {
 		$schema: 'http://json-schema.org/draft-04/schema#',
-		type: 'number',
+		type: 'integer',
+		format: 'int64',
+		minimum: 0 - Math.pow(2, 63),
+		maximum: Math.pow(2, 63) - 1
 	};
 
-	assert.deepEqual(result, expected, 'number type untouched');
+	assert.deepEqual(result, expected, 'int64 converted to minimum|maximum');
+});
+
+test('handles float format', function(assert) {
+	var schema
+		, result
+		, expected
+	;
+
+	assert.plan(1);
 
 	schema = {
 		type: 'number',
@@ -70,8 +72,36 @@ test('handles number types', function(assert) {
 	expected = {
 		$schema: 'http://json-schema.org/draft-04/schema#',
 		type: 'number',
-		format: 'float'
+		format: 'float',
+		minimum: 0 - Math.pow(2, 128),
+		maximum: Math.pow(2, 128) - 1
 	};
 
-	assert.deepEqual(result, expected, 'number type and format untouched');
+	assert.deepEqual(result, expected, 'float converted to minimum|maximum');
+});
+
+test('handles double format', function(assert) {
+	var schema
+		, result
+		, expected
+	;
+
+	assert.plan(1);
+
+	schema = {
+		type: 'number',
+		format: 'double'
+	};
+
+	result = convert(schema);
+
+	expected = {
+		$schema: 'http://json-schema.org/draft-04/schema#',
+		type: 'number',
+		format: 'double',
+		minimum: 0 - Number.MAX_VALUE,
+		maximum: Number.MAX_VALUE
+	};
+
+	assert.deepEqual(result, expected, 'double converted to minimum|maximum');
 });
--- test/string_types.test.js
+++ test/string_types.test.js
@@ -63,6 +63,31 @@ test('handling date', function(assert) {
 	assert.deepEqual(result, expected, 'date converted to date-time');
 });
 
+test('handles byte format', function(assert) {
+	var schema
+		, result
+		, expected
+	;
+
+	assert.plan(1);
+
+	schema = {
+		type: 'string',
+		format: 'byte'
+	};
+
+	result = convert(schema);
+
+	expected = {
+		$schema: 'http://json-schema.org/draft-04/schema#',
+		type: 'string',
+		format: 'byte',
+		pattern: '^[\\w\\d+\\/=]*$'
+	};
+
+	assert.deepEqual(result, expected, 'byte converted to pattern');
+});
+
 test('retaining custom formats', function(assert) {
 	var schema
 		, result
@@ -73,7 +98,7 @@ test('retaining custom formats', function(assert) {
 
 	schema = {
 		type: 'string',
-		format: 'email'
+		format: 'custom_email'
 	};
 
 	result = convert(schema);
@@ -81,8 +106,56 @@ test('retaining custom formats', function(assert) {
 	expected = {
 		$schema: 'http://json-schema.org/draft-04/schema#',
 		type: 'string',
-		format: 'email'
+		format: 'custom_email'
 	};
 
-	assert.deepEqual(result, expected, 'email retained');
+	assert.deepEqual(result, expected, 'custom format retained');
+});
+
+test('retain password format', function(assert) {
+	var schema
+		, result
+		, expected
+	;
+
+	assert.plan(1);
+
+	schema = {
+		type: 'string',
+		format: 'password'
+	};
+
+	result = convert(schema);
+
+	expected = {
+		$schema: 'http://json-schema.org/draft-04/schema#',
+		type: 'string',
+		format: 'password'
+	};
+
+	assert.deepEqual(result, expected, 'password format retained');
+});
+
+test('retain binary format', function(assert) {
+	var schema
+		, result
+		, expected
+	;
+
+	assert.plan(1);
+
+	schema = {
+		type: 'string',
+		format: 'binary'
+	};
+
+	result = convert(schema);
+
+	expected = {
+		$schema: 'http://json-schema.org/draft-04/schema#',
+		type: 'string',
+		format: 'binary'
+	};
+
+	assert.deepEqual(result, expected, 'binary format retained');
 });

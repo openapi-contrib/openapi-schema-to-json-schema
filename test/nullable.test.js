841d56c92092e40485682747457b724974cec7f6
Mika Kunnas <mika.kunnas@viidakko.fi>
2018-08-01 20:36:50 +0300

    Inject null in enum (if there's one) if nullable is true

--- test/nullable.test.js
+++ test/nullable.test.js
@@ -2,7 +2,7 @@ var test = require('tape')
 	, convert = require('../')
 ;
 
-test('handles nullable', function(assert) {
+test('handles nullable without enum', function(assert) {
 	var schema
 		, result
 		, expected
@@ -34,6 +34,47 @@ test('handles nullable', function(assert) {
 	expected = {
 		$schema: 'http://json-schema.org/draft-04/schema#',
 		type: 'string'
+	};
+
+	assert.deepEqual(result, expected, 'nullable removed, type untouched');
+});
+
+test('handles nullable with enum', function(assert) {
+	var schema
+		, result
+		, expected
+	;
+
+	assert.plan(2);
+
+	schema = {
+		type: 'string',
+		enum: ['a', 'b'],
+		nullable: true
+	};
+
+	result = convert(schema);
+
+	expected = {
+		$schema: 'http://json-schema.org/draft-04/schema#',
+		type: ['string', 'null'],
+		enum: ['a', 'b', null]
+	};
+
+	assert.deepEqual(result, expected, 'nullable converted');
+
+	schema = {
+		type: 'string',
+		enum: ['a', 'b'],
+		nullable: false,
+	};
+
+	result = convert(schema);
+
+	expected = {
+		$schema: 'http://json-schema.org/draft-04/schema#',
+		type: 'string',
+		enum: ['a', 'b'],
 	};
 
 	assert.deepEqual(result, expected, 'nullable removed, type untouched');
